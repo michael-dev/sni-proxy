c51d1a3fee731b4dd06b18d86706ef0960d34d46
Michael Braun <michael-dev@fami-braun.de>
2018-04-13 10:48:01 +0200

    add debian

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(sni-proxy):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="sni-proxy" command="/usr/bin/sni-proxy"
---
+++ debian/sni-proxy.default.ex
@@ -0,0 +1,10 @@
+# Defaults for sni-proxy initscript
+# sourced by /etc/init.d/sni-proxy
+# installed at /etc/default/sni-proxy by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
